8c0aa1745b7a45af98e64d7d3a5fde1dc1ef77b8
NeverDieOne <38184869+NeverDieOne@users.noreply.github.com>
2019-03-24 21:07:10 +0300

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -8,7 +8,7 @@ def save_hubble_pic_by_id(image_id):
     url = f'http://hubblesite.org/api/v3/image/{image_id}'
     response = requests.get(url)
     url_for_save = response.json()['image_files'][-1]['file_url']
-    extansion_of_image = utils.get_extansion_of_image(url_for_save)
+    extension_of_image = utils.get_extension_of_image(url_for_save)
     path = f'hubble{image_id}.{extansion_of_image}'
     utils.save_picture(url_for_save, path)
 
